3e354090adc83afc6d74a1ec5a09e33d1787fbf7
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-09-20 09:47:46 +0300

    Use tabs for indentation

--- spec/jsverifySpec.js
+++ spec/jsverifySpec.js
@@ -1,158 +1,158 @@
 /* global jsc, _, describe, it, expect, beforeEach */
 (function () {
-  "use strict";
+	"use strict";
 
-  beforeEach(function () {
-    this.addMatchers({
-      // Expects that property is synchronous
-      toHold: function () {
-        var actual = this.actual;
-        var notText = this.isNot ? " not" : "";
+	beforeEach(function () {
+		this.addMatchers({
+			// Expects that property is synchronous
+			toHold: function () {
+			var actual = this.actual;
+			var notText = this.isNot ? " not" : "";
 
-        var r = jsc.check(actual);
+			var r = jsc.check(actual);
 
-        var counterExampleText = r === true ? "" : "Counter example found: " + JSON.stringify(r.counterexample);
+			var counterExampleText = r === true ? "" : "Counter example found: " + JSON.stringify(r.counterexample);
 
-        this.message = function() {
-            return "Expected property to " + notText + " to not hold." + counterExampleText;
-        };
+			this.message = function() {
+				return "Expected property to " + notText + " to not hold." + counterExampleText;
+			};
 
-        return r === true;
-      },
-    });
-  });
+			return r === true;
+			},
+		});
+	});
 
-  describe("examples", function () {
-    it("failing inc", function () {
-      function inc(i) {
-          return i + 1;
-      }
+	describe("examples", function () {
+		it("failing inc", function () {
+			function inc(i) {
+				return i + 1;
+			}
 
-      var prop = jsc.forall(jsc.integer(), function (i) {
-          return inc(i) === i + 2;
-      });
+			var prop = jsc.forall(jsc.integer(), function (i) {
+				return inc(i) === i + 2;
+			});
 
-      expect(prop).not.toHold();
-    });
+			expect(prop).not.toHold();
+		});
 
-    it("fixed inc", function () {
-      function inc(i) {
-          return i + 1;
-      }
+		it("fixed inc", function () {
+			function inc(i) {
+				return i + 1;
+			}
 
-      var prop = jsc.forall(jsc.integer(), function (i) {
-          return inc(i) === i + 1;
-      });
+			var prop = jsc.forall(jsc.integer(), function (i) {
+				return inc(i) === i + 1;
+			});
 
-      expect(prop).toHold();
-    });
+			expect(prop).toHold();
+		});
 
-    it("failing add", function () {
-      function add(i, j) {
-          return i + (j && 1);
-      }
+		it("failing add", function () {
+			function add(i, j) {
+				return i + (j && 1);
+			}
 
-      var prop = jsc.forall(jsc.integer(), function (i) {
-          return jsc.forall(jsc.integer(), function (j) {
-              return add(i, j) === i + j;
-          });
-      });
+			var prop = jsc.forall(jsc.integer(), function (i) {
+				return jsc.forall(jsc.integer(), function (j) {
+					return add(i, j) === i + j;
+				});
+			});
 
-      expect(prop).not.toHold();
-    });
+			expect(prop).not.toHold();
+		});
 
-    it("other failing add", function () {
-      function add(i, j) {
-        return i + (j && 1);
-      }
+		it("other failing add", function () {
+			function add(i, j) {
+			return i + (j && 1);
+			}
 
-      var prop = jsc.forall(jsc.pair(jsc.integer(), jsc.integer()), function (p) {
-        var i = p[0];
-        var j = p[1];
-        return add(i, j) === i + j;
-      });
+			var prop = jsc.forall(jsc.pair(jsc.integer(), jsc.integer()), function (p) {
+			var i = p[0];
+			var j = p[1];
+			return add(i, j) === i + j;
+			});
 
-      expect(prop).not.toHold();
-    });
+			expect(prop).not.toHold();
+		});
 
-    it("fixed add", function () {
-      function add(i, j) {
-          return i + j;
-      }
+		it("fixed add", function () {
+			function add(i, j) {
+				return i + j;
+			}
 
-      var prop = jsc.forall(jsc.integer(), function (i) {
-          return jsc.forall(jsc.integer(), function (j) {
-              return add(i, j) === i + j;
-          });
-      });
+			var prop = jsc.forall(jsc.integer(), function (i) {
+				return jsc.forall(jsc.integer(), function (j) {
+					return add(i, j) === i + j;
+				});
+			});
 
-      expect(prop).toHold();
-    });
+			expect(prop).toHold();
+		});
 
-    it("failing add3", function () {
-      function add(i, j, k) {
-          return i + (j && 1) + k;
-      }
+		it("failing add3", function () {
+			function add(i, j, k) {
+				return i + (j && 1) + k;
+			}
 
-      var prop = jsc.forall(jsc.integer(), function (i) {
-          return jsc.forall(jsc.integer(), function (j) {
-              return jsc.forall(jsc.integer(), function (k) {
-                  return add(i, j, k) === i + j + k;
-              });
-          });
-      });
+			var prop = jsc.forall(jsc.integer(), function (i) {
+				return jsc.forall(jsc.integer(), function (j) {
+					return jsc.forall(jsc.integer(), function (k) {
+						return add(i, j, k) === i + j + k;
+					});
+				});
+			});
 
-      expect(prop).not.toHold();
-    });
+			expect(prop).not.toHold();
+		});
 
-    it("intersects", function () {
-      function contains(arr, x) {
-          return arr.indexOf(x) !== -1;
-      }
+		it("intersects", function () {
+			function contains(arr, x) {
+				return arr.indexOf(x) !== -1;
+			}
 
-      function intersects(a, b) {
-          return a.some(function (x) {
-              return contains(b, x);
-          });
-      }
+			function intersects(a, b) {
+				return a.some(function (x) {
+					return contains(b, x);
+				});
+			}
 
-      var prop = jsc.forall(jsc.nonshrinklist(), function (a) {
-          return jsc.forall(jsc.nonshrinklist(), function (b) {
-              return intersects(a, b) === (_.intersection(a, b) !== []);
-          });
-      });
+			var prop = jsc.forall(jsc.nonshrinklist(), function (a) {
+				return jsc.forall(jsc.nonshrinklist(), function (b) {
+					return intersects(a, b) === (_.intersection(a, b) !== []);
+				});
+			});
 
-      expect(prop).not.toHold();
+			expect(prop).not.toHold();
 
-      var prop2 = jsc.forall(jsc.list(), function (a) {
-          return jsc.forall(jsc.list(), function (b) {
-              return intersects(a, b) === (_.intersection(a, b) !== []);
-          });
-      });
+			var prop2 = jsc.forall(jsc.list(), function (a) {
+				return jsc.forall(jsc.list(), function (b) {
+					return intersects(a, b) === (_.intersection(a, b) !== []);
+				});
+			});
 
-      expect(prop2).not.toHold();
+			expect(prop2).not.toHold();
 
-      var prop3 = jsc.forall(jsc.list(), function (a) {
-          return jsc.forall(jsc.list(), function (b) {
-              return intersects(a, b) === (_.intersection(a, b).length !== 0);
-          });
-      });
+			var prop3 = jsc.forall(jsc.list(), function (a) {
+				return jsc.forall(jsc.list(), function (b) {
+					return intersects(a, b) === (_.intersection(a, b).length !== 0);
+				});
+			});
 
-      expect(prop3).toHold();
+			expect(prop3).toHold();
 
-      /*
-      var prop4 = jsc.forall(jsc.list(), function (a) {
-          return jsc.forall(jsc.list(), function (b) {
-              return q.delay(10).then(function () {
-                  return intersects(a, b) === (_.intersection(a, b).length !== 0);
-              });
-          });
-      });
+			/*
+			var prop4 = jsc.forall(jsc.list(), function (a) {
+				return jsc.forall(jsc.list(), function (b) {
+					return q.delay(10).then(function () {
+						return intersects(a, b) === (_.intersection(a, b).length !== 0);
+					});
+				});
+			});
 
-      jsc.check(prop4).then(function (res) {
-          console.log("intersects try 4:", res);
-      });
-      */
-    });
-  });
+			jsc.check(prop4).then(function (res) {
+				console.log("intersects try 4:", res);
+			});
+			*/
+		});
+	});
 }());
