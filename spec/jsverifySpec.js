0e589f44d26ddd93c0e73e28988f587129897171
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-09-23 08:26:15 +0300

    promise example

--- spec/jsverifySpec.js
+++ spec/jsverifySpec.js
@@ -1,4 +1,4 @@
-/* global jsc, _, describe, it, expect */
+/* global jsc, _, Q, describe, it, expect, waitsFor, runs */
 (function () {
 	"use strict";
 
@@ -25,6 +25,31 @@
 			});
 
 			expect(prop).toHold();
+		});
+
+		it("fixed inc - promise", function () {
+			function inc(i) {
+				return i + 1;
+			}
+
+			var propPromise = Q.delay(100).then(function () {
+				return jsc.forall(jsc.integer(), function (i) {
+					return inc(i) === i + 1;
+				});
+			});
+
+			// TODO: how to make matcher on promise?
+			var done = false;
+			propPromise.fin(function () { done = true; });
+			waitsFor(function () { return done; });
+
+			runs(function () {
+				propPromise.then(function (prop) {
+					expect(prop).toHold();
+				}, function (e) {
+					expect(false).toBe(true); // should be never executed
+				});
+			});
 		});
 
 		it("failing add", function () {
