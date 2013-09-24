516e9896de17fc0e45bb27e84244691de6b82bc5
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-09-24 10:29:28 +0300

    Reindent jasmineHelpers.js

--- speclib/jasmineHelpers.js
+++ speclib/jasmineHelpers.js
@@ -2,20 +2,20 @@ beforeEach(function () {
 	this.addMatchers({
 		// Expects that property is synchronous
 		toHold: function () {
-		var actual = this.actual;
-		var notText = this.isNot ? " not" : "";
-		/* global window */
-		var quiet = window && !(/verbose=true/).test(window.location.search);
+			var actual = this.actual;
+			var notText = this.isNot ? " not" : "";
+			/* global window */
+			var quiet = window && !(/verbose=true/).test(window.location.search);
 
-		var r = jsc.check(actual, { quiet: quiet });
+			var r = jsc.check(actual, { quiet: quiet });
 
-		var counterExampleText = r === true ? "" : "Counter example found: " + JSON.stringify(r.counterexample);
+			var counterExampleText = r === true ? "" : "Counter example found: " + JSON.stringify(r.counterexample);
 
-		this.message = function() {
-			return "Expected property to " + notText + " to not hold." + counterExampleText;
-		};
+			this.message = function() {
+				return "Expected property to " + notText + " to not hold." + counterExampleText;
+			};
 
-		return r === true;
+			return r === true;
 		},
 	});
 });
