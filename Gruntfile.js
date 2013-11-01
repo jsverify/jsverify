0a4a88c9e24961959a193cd7d205577a7c392393
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-11-01 10:06:57 +0200

    Enforce double quotmarks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,71 +1,71 @@
 /* jshint node: true */
-'use strict';
+"use strict";
 
 module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		jasmine: {
 			jsverify: {
-				src: 'lib/**/*.js',
+				src: "lib/**/*.js",
 				options: {
-					specs: 'spec/jsverify/*Spec.js',
-					helpers: 'helpers/*.js'
+					specs: "spec/jsverify/*Spec.js",
+					helpers: "helpers/*.js"
 				},
 			},
 			q: {
-				src: [ 'lib/**/*.js', 'dep/q.js' ],
+				src: [ "lib/**/*.js", "dep/q.js" ],
 				options: {
-					specs: 'spec/q/*Spec.js',
-					helpers: 'helpers/*.js'
+					specs: "spec/q/*Spec.js",
+					helpers: "helpers/*.js"
 				},
 			},
 			underscore: {
-				src: [ 'lib/**/*.js', 'dep/underscore.js' ],
+				src: [ "lib/**/*.js", "dep/underscore.js" ],
 				options: {
-					specs: 'spec/underscore/*Spec.js',
-					helpers: 'helpers/*.js'
+					specs: "spec/underscore/*Spec.js",
+					helpers: "helpers/*.js"
 				},
 			},
 			lodash: {
-				src: [ 'lib/**/*.js', 'dep/lodash.underscore.js' ],
+				src: [ "lib/**/*.js", "dep/lodash.underscore.js" ],
 				options: {
-					specs: 'spec/underscore/*Spec.js',
-					helpers: 'helpers/*.js'
+					specs: "spec/underscore/*Spec.js",
+					helpers: "helpers/*.js"
 				},
 			},
 			all: {
-				src: [ 'lib/**/*.js', 'dep/underscore.js', 'dep/q.js' ],
+				src: [ "lib/**/*.js", "dep/underscore.js", "dep/q.js" ],
 				options: {
-					specs: 'spec/**/*Spec.js',
-					helpers: 'helpers/*.js'
+					specs: "spec/**/*Spec.js",
+					helpers: "helpers/*.js"
 				},
 			},
 		},
 		jshint: {
 			options: {
-				jshintrc: '.jshintrc'
+				jshintrc: ".jshintrc"
 			},
 			gruntfile: {
-				src: 'Gruntfile.js',
+				src: "Gruntfile.js",
 				options: {
 					node: true,
 				},
 			},
 			lib: {
-				src: ['lib/**/*.js']
+				src: ["lib/**/*.js"]
 			},
 			spec: {
-				src: ['spec/**/*.js']
+				src: ["spec/**/*.js"]
 			}
 		},
 		watch: {
 			gruntfile: {
-				files: '<%= jshint.gruntfile.src %>',
-				tasks: ['jshint:gruntfile']
+				files: "<%= jshint.gruntfile.src %>",
+				tasks: ["jshint:gruntfile"]
 			},
 			lib: {
-				files: '<%= jshint.lib.src %>',
-				tasks: ['jshint:lib']
+				files: "<%= jshint.lib.src %>",
+				tasks: ["jshint:lib"]
 			},
 		},
 		literate: {
@@ -74,13 +74,13 @@ module.exports = function(grunt) {
 	});
 
 	// Tasks
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-jasmine');
-	grunt.loadNpmTasks('grunt-literate');
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-jasmine");
+	grunt.loadNpmTasks("grunt-literate");
 
 	// Default task.
-	grunt.registerTask('default', ['jshint']);
-	grunt.registerTask('test', ['jshint', 'jasmine:all', 'jasmine:lodash']);
-	grunt.registerTask('jasmine-build', ['jasmine:all:build']);
+	grunt.registerTask("default", ["jshint"]);
+	grunt.registerTask("test", ["jshint", "jasmine:all", "jasmine:lodash"]);
+	grunt.registerTask("jasmine-build", ["jasmine:all:build"]);
 };
