5c58787e82f60b3397ededdfaa2ec79264a9b63e
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-11-16 15:05:53 +0200

    Migrate miscspec

--- test/utility.js
+++ test/utility.js
@@ -26,6 +26,12 @@ describe("utility functions: _", function () {
 			assert(predicate({ a: 2, b: 2 }, { a: 1, b: 3 }));
 			assert(predicate({ a: 1}, { b: 1 }));
 		});
+
+		it("is reflexive", function () {
+			jsc.assert(jsc.forall(jsc.value(), function (x) {
+				return jsc._.isEqual(x, x);
+			}));
+		});
 	});
 
 	describe("FMap", function () {
@@ -34,6 +40,24 @@ describe("utility functions: _", function () {
 			m.insert([0], 1);
 			m.insert([0], 2);
 			assert.strictEqual(m.get([0]), 2);
+		});
+
+		it("works as object for integer keys", function () {
+			var prop = jsc.forall(jsc.array(jsc.pair(jsc.integer())), function (l) {
+				var m = new jsc._.FMap();
+				var o = {};
+
+				l.forEach(function (p) {
+					m.insert(p[0], p[1]);
+					o[p[0]] = p[1];
+				});
+
+				return Object.keys(o).every(function (k) {
+					return o[k] === m.get(parseInt(k, 10));
+				});
+			});
+
+			jsc.assert(prop);
 		});
 	});
 });
