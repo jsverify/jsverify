ab5ffd751d2a25b575edace14185bb988b377b67
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-11-16 14:56:41 +0200

    Many small changes, bug fixes to jsverify

--- spec/jsverify/miscSpec.js
+++ spec/jsverify/miscSpec.js
@@ -3,16 +3,6 @@
 	"use strict";
 
 	describe("miscellaneous functions", function () {
-		describe("id", function () {
-			it("forall x, x = id x", function () {
-				var prop = jsc.forall(jsc.value(), function (x) {
-					return jsc._.isEqual(x, jsc._.id(x));
-				});
-
-				expect(prop).toHold();
-			});
-		});
-
 		describe("isEqual", function () {
 			it("is reflexive", function () {
 				var prop = jsc.forall(jsc.value(), function (x) {
