fe14e2a878686437f41b95da08aaf556f2c01e36
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-03-24 19:03:31 +0200

    Use Makefile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,68 +4,13 @@
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
-        simplemocha: {
-            options: {
-                timeout: 3000,
-                ui: "bdd",
-                reporter: "spec"
-            },
-
-            all: { src: "test/**/*.js" }
-        },
-        jasmine: {
-            jsverify: {
-                src: "dist/jsverify.standalone.js",
-                options: {
-                    specs: "spec/*.js",
-                    helpers: "helpers/*.js"
-                },
-            },
-        },
-        jshint: {
-            options: {
-                jshintrc: ".jshintrc"
-            },
-            gruntfile: {
-                src: "Gruntfile.js",
-                options: {
-                    node: true,
-                },
-            },
-            lib: {
-                src: ["lib/**/*.js"]
-            },
-            spec: {
-                src: ["spec/**/*.js"]
-            },
-            test: {
-                src: ["test/**/*.js"],
-            },
-        },
-        watch: {
-            gruntfile: {
-                files: "<%= jshint.gruntfile.src %>",
-                tasks: ["jshint:gruntfile"]
-            },
-            lib: {
-                files: "<%= jshint.lib.src %>",
-                tasks: ["jshint:lib"]
-            },
-        },
         literate: {
             "README.md": "lib/jsverify.js",
         },
     });
 
-    // Tasks
-    grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-jasmine");
-    grunt.loadNpmTasks("grunt-simple-mocha");
     grunt.loadNpmTasks("grunt-literate");
 
     // Default task.
-    grunt.registerTask("default", ["jshint"]);
-    grunt.registerTask("test", ["jshint", "simplemocha", "jasmine"]);
-    grunt.registerTask("jasmine-build", ["jasmine:jsverify:build"]);
+    grunt.registerTask("default", ["literate"]);
 };
