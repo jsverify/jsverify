a1b33358bd9eb4640fad520dc4c699dfea3780d8
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-03-24 19:18:39 +0200

    Add documentation about --jsverifyRngState

---
+++ examples/nat.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var jsc = require("../lib/jsverify.js");
+
+describe("natural numbers", function () {
+  it("are greater than or equal to 0", function () {
+    var property = jsc.forall(jsc.nat(), function (n) {
+      return n >= 0;
+    });
+
+    jsc.assert(property);
+  });
+
+  it("are less than 90", function () {
+    var property = jsc.forall(jsc.nat(), function (n) {
+      return n < 90;
+    });
+
+    jsc.assert(property, { size: 100 });
+  });
+});
