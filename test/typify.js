deb980820d521856e09fc187cbc4e453c827b573
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-10-01 09:35:53 +0300

    higher order types

--- test/typify.js
+++ test/typify.js
@@ -3,7 +3,7 @@
 "use strict";
 
 var jsc = require("../lib/jsverify.js");
-// var _ = require("underscore");
+var _ = require("underscore");
 var assert = require("assert");
 
 describe("typify", function () {
@@ -11,6 +11,15 @@ describe("typify", function () {
     it("integer", function () {
       jsc.assert(jsc.forall("integer", function (i) {
         return Math.round(i) === i;
+      }));
+    });
+  });
+
+  describe("compositional", function () {
+    it("sort is idempotent", function () {
+      jsc.assert(jsc.forall("array integer", function (arr) {
+        var sorted= _.sortBy(arr);
+        return _.isEqual(_.sortBy(sorted), sorted);
       }));
     });
   });
