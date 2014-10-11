3aadfcd614f953d1f2d792d07c2e53726c2b847e
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-10-11 16:43:07 +0300

    char, asciichar and asciistring

--- test/shrink.js
+++ test/shrink.js
@@ -81,6 +81,26 @@ describe("shrink", function () {
     });
   });
 
+  describe("string", function () {
+    it("shrink of empty string is empty", function () {
+      assert(jsc.string().shrink("").length === 0);
+    });
+
+    it("shrinked array contains empty string for non-empty input", function () {
+      assert(jsc.string().shrink("foobar").indexOf("") !== -1);
+    });
+  });
+
+  describe("asciistring", function () {
+    it("shrink of empty asciistring is empty", function () {
+      assert(jsc.asciistring.shrink("").length === 0);
+    });
+
+    it("shrinked array contains empty asciistring for non-empty input", function () {
+      assert(jsc.asciistring.shrink("foobar").indexOf("") !== -1);
+    });
+  });
+
   describe("nonshrink array", function () {
     it("cannot be shrinked", function () {
       var property = jsc.forall(jsc.nonshrink(jsc.array(jsc.nat())), function (arr) {
