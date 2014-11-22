0e0bcd74f12fbf01ea7c6a39d66240480acdf13d
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-11-22 21:36:37 +0200

    Add jsc.property fact test

---
+++ test/jsverify.js
@@ -0,0 +1,29 @@
+/* jshint node:true */
+/* global describe, it:true */
+"use strict";
+
+var jsc = require("../lib/jsverify.js");
+var assert = require("assert");
+
+describe("jsverify", function () {
+  describe("property", function () {
+    jsc.property("+0 === -0", function () {
+      return +0 === -0;
+    });
+
+    it("fail case", function () {
+      var origIt = it;
+      it = function (name, p) {
+        assert.throws(p);
+      };
+
+      jsc.property("0 === 1", function () {
+        /* eslint-disable */
+        return 0 === 1;
+        /* eslint-enable */
+      });
+
+      it = origIt;
+    });
+  });
+});
