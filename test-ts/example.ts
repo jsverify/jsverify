902226e33a1ad7cdfcdac287ffdefa331c581572
Jan van Brügge <supermanitu@gmail.com>
2017-03-29 21:36:43 +0200

    Add async test

--- test-ts/example.ts
+++ test-ts/example.ts
@@ -6,4 +6,17 @@ describe("basic jsverify usage", () => {
   jsc.property("boolean fn thrice", jsc.fn(jsc.bool), jsc.bool, (f, b) =>
     f(f(f(b))) === f(b)
   );
+
+    it("async evaluation has no effect on pure computation", done => {
+        const property = jsc.forall(jsc.fn(jsc.nat), jsc.json, jsc.nat(20), (f, x, t) => {
+            const sync = f(x);
+            return new Promise(resolve => {
+                setTimeout(() => resolve(f(x)), t);
+            }).then(val => val === sync);
+        });
+
+        jsc.assert(property)
+            .then(val => val ? done(val) : done())
+            .catch(error => done(error));
+    });
 });
