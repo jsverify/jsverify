589ffff65b32a021dae4a94974fce31d33e8ba66
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-04-01 13:10:34 +0300

    Fix indentation

--- test-ts/example.ts
+++ test-ts/example.ts
@@ -7,16 +7,16 @@ describe("basic jsverify usage", () => {
     f(f(f(b))) === f(b)
   );
 
-    it("async evaluation has no effect on pure computation", done => {
-        const property = jsc.forall(jsc.fn(jsc.nat), jsc.json, jsc.nat(20), (f, x, t) => {
-            const sync = f(x);
-            return new Promise(resolve => {
-                setTimeout(() => resolve(f(x)), t);
-            }).then(val => val === sync);
-        });
-
-        jsc.assert(property)
-            .then(val => val ? done(val) : done())
-            .catch(error => done(error));
+  it("async evaluation has no effect on pure computation", done => {
+    const property = jsc.forall(jsc.fn(jsc.nat), jsc.json, jsc.nat(20), (f, x, t) => {
+      const sync = f(x);
+      return new Promise(resolve => {
+        setTimeout(() => resolve(f(x)), t);
+      }).then(val => val === sync);
     });
+
+    jsc.assert(property)
+      .then(val => val ? done(val) : done())
+      .catch(error => done(error));
+  });
 });
