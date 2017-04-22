0febf88828584b5064fde9b197af6a7a157c2c47
Andrew Kiellor <akiellor@@users.noreply.github.com>
2017-04-21 22:22:16 -0700

    Fail suchthat generator when failing to create good value

--- test/suchthat.js
+++ test/suchthat.js
@@ -4,6 +4,7 @@
 
 var jsc = require("../lib/jsverify.js");
 var arbitraryAssert = require("../lib/arbitraryAssert.js");
+var expect = require("chai").expect;
 
 describe("suchthat", function () {
   var arb = jsc.suchthat(jsc.integer, function (v) {
@@ -27,5 +28,19 @@ describe("suchthat", function () {
     jsc.assert(jsc.forall(arbAsString, function (value) {
       return parseInt(value, 10) % 2 === 0;
     }));
+  });
+
+  it("should fail after too many attempts at generating a value", function () {
+    var degenerateArb = jsc.suchthat(jsc.nat, function () {
+      return false;
+    });
+
+    try {
+      jsc.assert(jsc.forall(degenerateArb, function () {
+        return true;
+      }));
+    } catch (e) {
+      expect(e.message).to.equal("Too many attempts trying to generate a value.");
+    }
   });
 });
